d04cd780fd278ec6c584f60687e386b4149a5b36
johnthemarksman <johnthemarksman@aol.com>
2019-05-21 00:53:57 -0400

    got very basic mvp working now i want to add a moving average

--- weight.py
+++ weight.py
@@ -1,6 +1,9 @@
 import time
+
+
+
 ### User entered data
-startDate = (2019, 5, 17, 13, 3, 39, 0, 137, 1)  #year, month, day
+#startDate = (2019, 5, 17, 13, 3, 39, 0, 137, 1)  #year, month, day
 endDate = (2019, 5, 29)  #year, month, day
 startingWeight = 173.2  
 goalPerWeek = 3  #how many pounds you want to lose per week
@@ -11,20 +14,72 @@ goalPerHour = goalPerDay/24
 buffer = 2/24
 
 currentWeight = float(input("Enter your current weight: "))
-
 currentDate = time.localtime()
 
-#print(currentDate)
+try:
+    f = open("startDate.txt", "r")
+    f.close()
+    pass
+except:
+    f = open("startDate.txt", "w+")
+    for i in currentDate:
+        f.write(str(i) + ",")
+    f.write("\n")
+    #f.write(str(currentDate))
+    f.close()
+    pass
 
 
-requiredWeight = (startingWeight - (currentDate[7] - startDate[7])*goalPerDay) + buffer*currentDate[3] + (buffer/60)*currentDate[4]
 
-if(currentWeight < requiredWeight):
-    print("you can eat", currentWeight, requiredWeight)
-else:
-    print("you can not eat", currentWeight, requiredWeight)
+f = open("startDate.txt", "r")
+startDate = f.read().split(",")
+f.close()
+
+startDate.pop()
+for i in range(len(startDate) - 1):
+    startDate[i] = int(startDate[i])
+
+    
+# print(startDate)
+# print(currentDate)
 
 
-i = (currentWeight - requiredWeight)/(buffer/60)
+requiredWeight = (startingWeight - (currentDate[7] - startDate[7])*goalPerDay)# + buffer*currentDate[3] + (buffer/60)*currentDate[4]
+# if(currentWeight < requiredWeight):
+#     print("you can eat", currentWeight, requiredWeight)
+# else:
+#     print("you can not eat", currentWeight, requiredWeight)
 
-print("You can eat in", i, "minutes")
+
+# i = (currentWeight - requiredWeight)/(buffer/60)
+# print("You can eat in", i, "minutes")
+
+
+#start of intermittent fasting
+try:
+    f = open("startDate.txt", "r")
+    f.close()
+    pass
+except:
+    f = open("startDate.txt", "w+")
+    for i in currentDate:
+        f.write(str(i) + ",")
+    #f.write(str(currentDate))
+    f.close()
+    pass
+
+
+fastPeriod = 8
+maxDiff = 4
+diffIntervals = maxDiff/fastPeriod
+
+diff = currentWeight - requiredWeight
+
+fastPeriod = fastPeriod - int(diff/diffIntervals)
+
+print("you can eat for", fastPeriod, "hours today")
+
+
+
+
+
